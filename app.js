85560a89a2e9dba308c8a110fb5cfdb7a347b593
Pareja7 <Pareja7@users.noreply.github.com>
2021-10-04 19:50:22 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const linkedList = require('./Node')
+
+function reverseList(head){
+  let prev = null;
+  let next = null;
+
+  while(head != null){
+    head.next = prev;
+    prev = head;
+    head = next;
+  }
+return prev;
+
+
+
+console.log(reverseList(linkedList))
+
